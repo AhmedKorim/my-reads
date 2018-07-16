23dce6c315087b5d055ef833b5a632e8e001bddb
ahmedkorim <ahmedkorrim@gmail.com>
2018-07-16 10:53:43 -0700

    seatch works 100%

--- src/components/list-of-books/ListComponent.js
+++ src/components/list-of-books/ListComponent.js
@@ -3,40 +3,53 @@ import {Link} from "react-router-dom";
 import Book from "./BookComponent";
 
 class ListComponent extends React.Component {
-    componentDidMount(){
+    componentDidMount() {
     }
-    static bookShelfes =['Currently Reading','Currently Reading','Read']
+
+    static bookShelves = [{
+        name: 'Currently Reading'
+        , shelfTitle: "currentlyReading"
+    }
+        , {
+            name: 'Want To Read'
+            , shelfTitle: "wantToRead"
+        }, {
+            name: 'Read'
+            , shelfTitle: "read"
+        }];
+
     render() {
         return (
-        <div className="list-books">
 
-            <div className="list-books-title">
-                <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-                <div>
-                    {ListComponent.bookShelfes.map((bookshelf, index) =>(
-                        <div className="bookshelf" key={index}>
-                            <h2 className="bookshelf-title">Currently Reading</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    {console.log(this.props.allBooks)}
-                                    {this.props.allBooks.map((book, index) => (
-
-                                        <li key={index}>
-                                            <Book book={book}/>
-                                        </li>
-                                    ))}
-                                </ol>
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        {ListComponent.bookShelves.map((bookShelf, index) => (
+                            <div className="bookshelf" key={index}>
+                                <h2 className="bookshelf-title">{bookShelf.name}</h2>
+                                <div className="bookshelf-books">
+                                    <ol className="books-grid">
+                                        {this.props.allBooks
+                                            .filter(book => book.shelf === bookShelf.shelfTitle)
+                                            .map((bookData ,index) => (
+                                            <li key={index}>
+                                                    <Book bookData={bookData}/>
+                                                </li>))}
+                                    </ol>
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
+                </div>
+                <div className="open-search">
+                    <Link to={'/search'}>add a book</Link>
                 </div>
             </div>
-            <div className="open-search">
-                <Link to={'/search'}>add a book</Link>
-            </div>
-        </div>
-        ) }
+        )
+    }
 }
+
 export default ListComponent;
--- src/components/search-books/SearchBooksComponent.js
+++ src/components/search-books/SearchBooksComponent.js
@@ -1,29 +1,44 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {search} from "./../../BooksAPI";
+import Book from "../list-of-books/BookComponent";
 
-const SearchBooksComponent = (props) => {
-    return (
-        <div className="search-books">
-            <div className="search-books-bar">
-                <Link className="close-search" to="/">Close</Link>
-                <div className="search-books-input-wrapper">
-                    {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+class SearchBooksComponent extends React.Component {
+    state = {
+        query: '',
+        searchResults: [],
+    };
+    search = (value) => {
+        this.setState({query: value});
+        search(this.state.query).then(res => (
+            res.error ?   this.setState({searchResults: []}) : this.setState({searchResults: res})
+        )).catch(error => console.log(error))
+    };
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                    <input type="text" placeholder="Search by title or author"/>
+    render() {
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to="/">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author"
+                               value={this.state.query}
+                               onChange={event => this.search(event.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {console.log(this.state.searchResults)}
+
+                        {this.state.searchResults && this.state.searchResults.map((bookData, index) =>
+                            (<li key={index}><Book bookData={bookData}/></li>))}
+                    </ol>
                 </div>
             </div>
-            <div className="search-books-results">
-                <ol className="books-grid"></ol>
-            </div>
-        </div>
 
-    )
+        )
+    }
 }
 
 export default SearchBooksComponent;
